a9b8ebbe9623a781a2d229578d04a029c03c1ddd
andreandradearke <aandrade@arkesystems.com>
2025-07-24 19:30:03 -0300

    Authentication added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn, signOut } from '@/auth';
+import { AuthError } from 'next-auth';
+
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
@@ -115,4 +118,32 @@ export async function deleteInvoice(id: string) {
     console.error(error);
   }
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
+export async function logout(){
+  try {
+    await signOut({ redirectTo: '/' });
+  } catch (error) {
+    console.error('Failed to sign out:', error);
+    throw error;
+  }
 }
